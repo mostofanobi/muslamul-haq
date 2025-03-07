651e484664c8c6565de0cf9b00bc1a5f2ab55170
mostofanobi <mntanveer9196@gmail.com>
2025-03-07 14:12:44 +0600

    wip

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -5,18 +5,9 @@ import Link from 'next/link';
 const Hero = () => {
   return (
     <section className='relative'>
-      {/* <div className='top-22 pointer-events-none absolute left-1/2 aspect-square h-full -translate-x-1/2'>
-        <div className='absolute-center h-full w-full bg-[radial-gradient(50%_50%_at_50%_50%,rgba(0,92,244,.12)_0%,rgba(1,5,19,.12)_100%)]'></div>
-        <div className='absolute-center h-[40%] w-[40%] rounded-full border'></div>
-        <div className='absolute-center h-[60%] w-[60%] rounded-full border'></div>
-        <div className='absolute-center h-[80%] w-[80%] rounded-full border'></div>
-        <div className='absolute-center h-full w-full rounded-full border'></div>
-        <div className='absolute-center h-[120%] w-[120%] rounded-full border'></div>
-      </div> */}
-
-      <div className='mx-auto max-w-7xl px-6 pb-20 pt-40 lg:px-8'>
+      <div className='mx-auto max-w-7xl px-6 pb-20 pt-20 lg:px-8 lg:pt-32'>
         <div className='relative flex flex-col items-center text-center'>
-          <div className='flex items-center justify-center gap-4 text-[12px] font-medium leading-6 lg:justify-start'>
+          <div className='mb-5 flex items-center justify-center gap-4 text-[12px] font-medium leading-6 lg:justify-start'>
             <p>Video editor</p>
             <svg
               viewBox='0 0 2 2'
@@ -35,10 +26,10 @@ const Hero = () => {
             </svg>
             <p>Content creator</p>
           </div>
-          <h1 className='mb-6 max-w-6xl text-balance bg-gradient-to-b from-[#B8D3FA] to-[#E5EFFE] bg-clip-text text-6xl font-extralight text-transparent md:text-7xl md:leading-[1.25]'>
+          <h1 className='mb-8 max-w-6xl text-balance bg-gradient-to-b from-[#B8D3FA] to-[#E5EFFE] bg-clip-text text-5xl font-extralight text-transparent md:text-7xl md:leading-[1.25]'>
             Craft Epic Visual Stories That Captivate
           </h1>
-          <p className='mb-14 max-w-3xl text-pretty text-lg font-light leading-none sm:text-xl/8'>
+          <p className='mb-14 max-w-3xl text-pretty text-sm font-light sm:text-xl/8'>
             With a thriving video editing agency based in the BD and over 1000+
             client videos edited. Siam isn’t just editing videos; he’s shaping
             the future of digital content. Whether you’re here to learn, laugh,
